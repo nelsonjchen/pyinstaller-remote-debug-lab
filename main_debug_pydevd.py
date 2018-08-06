9858570f03a5988537ed7c7f768bce73c37d68ce
Nelson Chen <nelson.chen@mobivity.com>
2018-08-06 14:29:35 -0700

    PyDEVD version

--- main_debug_pydevd.py
+++ main_debug_pydevd.py
@@ -2,10 +2,8 @@ from __future__ import print_function
 from counterup import CounterPrint
 
 print("DEBUG VERSION")
-import ptvsd
-ptvsd.enable_attach("my_secret", address=("0.0.0.0", 3000))
-print("Waiting for Attach")
-ptvsd.wait_for_attach()
+import pydevd
+pydevd.settrace('localhost', port=3434, stdoutToServer=True, stderrToServer=True)
 
 if __name__ == "__main__":
     CounterPrint()
