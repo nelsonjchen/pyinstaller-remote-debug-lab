dd82716bb35fe1359ed8e22bec27aa9366b295eb
Nelson Chen <nelson.chen@mobivity.com>
2018-12-12 19:08:57 -0700

    Fix API for newer PTVSD

--- main_debug_ptvsd.py
+++ main_debug_ptvsd.py
@@ -3,8 +3,7 @@ from counterup import CounterPrint
 
 print("DEBUG VERSION")
 import ptvsd
-ptvsd.enable_attach("my_secret", address=("0.0.0.0", 3000))
-print("Waiting for Attach")
+ptvsd.enable_attach()
 ptvsd.wait_for_attach()
 
 if __name__ == "__main__":
